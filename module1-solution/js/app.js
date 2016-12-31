5a513b38aec8c1a577c1959e89089cdb61b7a502
upenderraneru <upenderraneru2015@gmail.com>
2016-12-30 20:22:06 -0600

    latest update

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -6,18 +6,14 @@
 
   function LunchCheckController($scope){
 
-    $scope.splitString = function() {
-
-      if(!($scope.dishesList)){
-      $scope.message = "Please enter data first";
+  $scope.splitString = function() {
+    if(!($scope.dishesList)){
+          $scope.message = "Please enter data first";
     }
       else{
-      var dishescount = $scope.dishesList.split(',');
-      $scope.message  = (dishescount.length > 3) ?  "Too much!" : "Enjoy!";
+          var dishescount = $scope.dishesList.split(',');
+          $scope.message  = (dishescount.length > 3) ?  "Too much!" : "Enjoy!";
+      }
     }
-    }
-
   }
-
-
 })();
